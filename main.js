406f28be082d6ef27777e14616114498518a3377
nemota <walkinggreencoordinator@gmail.com>
2020-09-07 20:53:52 +0900

    あああ

---
+++ main.js
@@ -0,0 +1,3 @@
+var video=document.getElementById("player_html5_api");
+var m3u8url=JSON.parse(video.dataset.moviePlaylist)[0].source.url);
+console.log('ffmpeg -i "'+m3u8url+'" -c copy -bsf:a aac_adtstoasc "output.mp4"');
